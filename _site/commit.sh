d964d1f14c8c4bf7c192a8fd96ea142e690e9f40
Nigel Charleston <ndcharle@umich.edu>
2020-03-02 17:57:54 -0500

    Added while loop to commit.sh; will let rsync run continuously in the background every 10 seconds.

--- _site/commit.sh
+++ _site/commit.sh
@@ -3,4 +3,8 @@
 DIRECTORY="~/test_project_blue"
 SOURCE="/mnt/c/users/123ch/Nigel2016.github.io/*"
 DESTINATION="ndcharle@oncampus-course.engin.umich.edu"
-sudo rsync -ruve ssh ${SOURCE} ${DESTINATION}:${DIRECTORY}
+
+while true; do
+    sudo rsync -ruve ssh ${SOURCE} ${DESTINATION}:${DIRECTORY}
+    sleep 10
+done
